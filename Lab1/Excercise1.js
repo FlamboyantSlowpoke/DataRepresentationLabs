155c7f96b43efa39f0c3924f19cb71b47d273d15
FlamboyantSlowpoke <benjaminstacey15@gmail.com>
2023-09-29 09:29:04 +0100

    added base example functions and arrow functions

---
+++ Lab1/Excercise1.js
@@ -0,0 +1,25 @@
+//G00402833
+//Written on OSX 14.0
+//Excercise 1 lab1
+
+console.log("Hello World");
+
+function hello() {
+    console.log("hi");
+}
+
+hello();
+
+
+//example of anonymous arrow function
+let sayBi = ()=> {
+    console.log("bye");
+}
+sayBi();
+
+
+let test = (myValue) => {
+    console.log(myValue);
+}
+
+test("Bonjour");
