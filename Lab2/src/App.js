299fabff54e9c7d9fddd5c0cd59531c2eca3b2b1
FlamboyantSlowpoke <benjaminstacey15@gmail.com>
2023-10-05 23:36:43 +0100

    Lab2 E2(A)

--- Lab2/src/App.js
+++ Lab2/src/App.js
@@ -7,8 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
+
+        {/* E2(a) */}
+        <h1>
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
