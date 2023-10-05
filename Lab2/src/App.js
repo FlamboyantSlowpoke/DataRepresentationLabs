075b26fb6f785fe445e65916978af79967ad3a4e
FlamboyantSlowpoke <benjaminstacey15@gmail.com>
2023-10-05 23:44:09 +0100

    Lab2 E2(b)

--- Lab2/src/App.js
+++ Lab2/src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        </p>
-
         {/* E2(a) */}
         <h1>
           Hello world!
         </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>
+          It is {new Date().toLocaleTimeString()}.
+        </h2>
       </header>
     </div>
   );
