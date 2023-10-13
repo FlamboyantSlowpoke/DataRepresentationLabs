c4e004a2ba78e4023dd8ae73a23f76ff88824cf1
Benjamin Stacey <benjaminstacey15@gmail.com>
2023-10-13 20:19:01 +0100

    Lab3 Made initial repository

--- Lab2/src/App.js
+++ Lab2/src/App.js
@@ -9,29 +9,22 @@ import Content from './components/content';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/content">Content</Nav.Link>
-              <Nav.Link as={Link} to="/header">Header</Nav.Link>
-              <Nav.Link as={Link} to="/footer">Footer</Nav.Link>
+              <Nav.Link href="/content">Content</Nav.Link>
+              <Nav.Link href="/header">Header</Nav.Link>
+              <Nav.Link href="/footer">Footer</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
@@ -39,15 +32,15 @@ function App() {
 
               <div className="App">
         <Routes>
-          <Route path="/content" element={<Content />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/footer" element={<Footer />} />
+          <Route path="/content" element={<Content />}></Route> 
+          <Route path="/header" element={<Header />}></Route> 
+          <Route path="/footer" element={<Footer />}></Route>
           <Route path="/" element={<h1>Welcome to Home</h1>} />
         </Routes>
       </div>
 
       </>
-    </Router>
+    </BrowserRouter>
   );
 }
 
