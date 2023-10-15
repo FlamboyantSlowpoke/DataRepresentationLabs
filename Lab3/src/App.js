42821bfb3b170e5e45fc019b0236da110acb655c
Benjamin Stacey <benjaminstacey15@gmail.com>
2023-10-15 18:41:12 +0100

    Lab3 E2 (c) added json, as well as books function

--- Lab3/src/App.js
+++ Lab3/src/App.js
@@ -2,8 +2,8 @@
 import './App.css';
 import React from 'react';
 // import Header from './components/header';
-import Header from './components/header';
-import Footer from './components/footer';
+import Read from './components/read';
+import Create from './components/create';
 import Content from './components/content';
 
 
@@ -14,17 +14,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//nav link estabishes routing to linked components 
 function App() {
   return (
     <BrowserRouter>
       <>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Brand href="/">Lab3</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/content">Content</Nav.Link>
-              <Nav.Link href="/header">Header</Nav.Link>
-              <Nav.Link href="/footer">Footer</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
@@ -33,8 +34,8 @@ function App() {
               <div className="App">
         <Routes>
           <Route path="/content" element={<Content />}></Route> 
-          <Route path="/header" element={<Header />}></Route> 
-          <Route path="/footer" element={<Footer />}></Route>
+          <Route path="/read" element={<Read />}></Route> 
+          <Route path="/create" element={<Create />}></Route>
           <Route path="/" element={<h1>Welcome to Home</h1>} />
         </Routes>
       </div>
--- Lab3/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-//E3 (c)
-//arrow function that returns the footer component
-const Footer = () => {
-    return <h1>My Footer is another component</h1>
-}
-
-export default Footer;
