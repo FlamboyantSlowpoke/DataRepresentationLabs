8b4d068f9cb825e2807862269ee67851afc22575
Benjamin Stacey <benjaminstacey15@gmail.com>
2023-10-20 10:23:34 +0100

    Lab 4 E3 (bp2)

--- Lab4/src/components/create.js
+++ Lab4/src/components/create.js
@@ -1,8 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 
 //Lab3 E2 (b)
 const Create = () => {
-    return <h3>Hello from create component</h3>
+
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [cover, setCover] = useState("");
+
+    const handleSubmit = (e) => {
+        //prevents page from refreshing
+        e.preventDefault();
+
+        //display user input to browser console
+        console.log("title " +title+ "Author :" +author+ "Cover: " +cover);
+    }
+
+    return (
+        //lab4 E3 (a) + E3 (b)
+
+        //creates form that accepts user input 
+        <div>
+            <form onSubmit ={handleSubmit}/>
+            <div className="form-group">
+                <label> Edit Book Title: </label>
+                <input type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }}
+                />
+            </div>
+            <div className="form-group">
+                <label> Edit Author: </label>
+                <input type="text"
+                    className="form-control"
+                    value={author}
+                    onChange={(e) => { setAuthor(e.target.value) }}
+                />
+            </div>
+            <div className="form-group">
+                <label> Edit Cover: </label>
+                <input type="text"
+                    className="form-control"
+                    value={cover}
+                    onChange={(e) => { setCover(e.target.value) }}
+                />
+            </div>
+            {/* button for user to commit changes */}
+            <input type="submit" value="Add Book"/>
+           
+        </div>
+
+    );
 }
 
 export default Create;
