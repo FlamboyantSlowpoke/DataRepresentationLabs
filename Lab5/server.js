20db187a7afa03d26d5a3d02379dc50aa51c25dc
Benjamin Stacey <benjaminstacey15@gmail.com>
2023-10-29 18:39:31 +0000

    Lab 5 ammended issues, added comments

--- Lab5/server.js
+++ Lab5/server.js
@@ -1,22 +1,25 @@
+
 const express = require('express')
 const app = express()
 const port = 4000
 
 //requires npm install body-parser
+//allows the use of req.body
 const bodyParser = require('body-parser');  
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+//defualt page displaying hello world
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+//returns the name given in the url
 app.get('/hello/:name', (req, res) => {
     console.log(req.params.name);
     res.send('Hello World!');
 })
-
+//returns json data
 app.get('/api/books', (req, res) => {
     console.log(req.params.name);
     const data = [
@@ -64,25 +67,25 @@ app.get('/api/books', (req, res) => {
 
 })
 
-//returns file index.html
+//returns file index.html 
 app.get('/test', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-//lists value of port in console
+//lists value of port in console 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
 
-// returns the values given from index.html
+// returns the values given from index.html in the console
 app.get('/name', (req, res) => {
-    console.log( "hello " +req.query.fname + " " + req.query.lname);
+    console.log( "hello " +req.query.fName + " " + req.query.lName);
 })
 
-//post is more secure for sending informationn
+//post is more secure for sending information to the server
 app.post('/name', (req, res) => {
-    res.send('Got a POST request ' +req.body.fname + " " + req.body.lname);
+    res.send('Got a POST request ' +req.body.fName + " " + req.body.lName);
 })
 
 
