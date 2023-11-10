df30b3515740d994f9ee32cacfd5999e49a0448e
Benjamin Stacey <benjaminstacey15@gmail.com>
2023-11-10 09:27:23 +0000

    Lab6 added cors and res function to server

--- Lab6/backEnd/server.js
+++ Lab6/backEnd/server.js
@@ -2,12 +2,14 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const cors = require('cors');
 
 //requires npm install body-parser
 //allows the use of req.body
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 //defualt page displaying hello world
@@ -88,5 +90,12 @@ app.post('/name', (req, res) => {
     res.send('Got a POST request ' + req.body.fName + " " + req.body.lName);
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
 
 
